5748e3ed270bc88d6832aec38f23d7a22bfa4983
Ignacio Rodrigo <irodrigo17@gmail.com>
2013-01-05 13:36:41 -0200

    Add basic validation to the User model.

--- rails-server/microblog/app/models/user.rb
+++ rails-server/microblog/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password
   has_many :messages
+
+  validates :name,  :presence => true
+  validates :email, :presence => true, 
+  					:uniqueness => true,
+  					:format => { 
+  						:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, 
+  						:message => "Must be a valid email address"}
+  validates :password, :presence => true
 end
